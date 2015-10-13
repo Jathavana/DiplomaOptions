4839675dd2d71725daeadb255f0fd057d7638c41
Lukasz Pacyk <lpacyk31@gmail.com>
2015-10-12 18:10:34 -0700

    Wedding Commitments

--- DiplomaDataModel/Choice.cs
+++ DiplomaDataModel/Choice.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace DiplomaDataModel
 {
@@ -37,6 +38,7 @@ namespace DiplomaDataModel
         //No duplicate rows allowed for the same year term
         [Display(Name = "Student Id")]
         [RegularExpression("A00\\d{6}", ErrorMessage="Invalid Student ID, A00123456 is the expected format")]
+        [RemoteAttribute("IsUserExists", "Validate", ErrorMessage = "This shit is dope")]
         public string StudentId { get; set; }
 
         [Required]
---
+++ DiplomaOptions/Controllers/ValidateController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiplomaOptions.Controllers
+{
+    public class ValidateController : Controller
+    {
+        public JsonResult IsUserExists(string userId)
+            {
+                return IsExist(userId) ? Json(true, JsonRequestBehavior.AllowGet) : Json(false, JsonRequestBehavior.AllowGet);
+            }
+
+            public bool IsExist(string userId)
+            {
+                // Write code to validate UserId weather is Userid already exists or not and base on return true and false.  
+                if (string.IsNullOrEmpty(userId)) return false;
+                else if (userId == "LilJayJay") return false;
+                else return true;
+            }
+    }
+}
